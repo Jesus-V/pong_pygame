cea9b601059150e2730102336518ce94b42f48f7
Jesus Violante <jesusviolante17@gmail.com>
2024-03-12 13:24:20 -0500

    Fixed a small bug
    Game kept crashing due to a typo in line 19. Fixed Typo and game runs fine now

--- pong.py
+++ pong.py
@@ -16,25 +16,26 @@ ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2- 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height / 2 -70, 10, 140)
 
-bg_color = pygame.color('grey12')
+bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
 
 
 
 while True:
-
-    #visuals
-    pygame.draw.rect(screen,light_grey,player)
-    pygame.draw.rect(screen,light_grey,opponent)
-    pygame.draw.ellipse(screen,light_grey,ball)
-
     #handling input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    #visuals
+    screen.fill(bg_color)
+    pygame.draw.rect(screen,light_grey,player)
+    pygame.draw.rect(screen,light_grey,opponent)
+    pygame.draw.ellipse(screen,light_grey,ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0), (screen_width / 2, screen_height))
 
+    
     #Updating the window
-        pygame.display.flip()
-        clock.tick(60)
+    pygame.display.flip()
+    clock.tick(60)
